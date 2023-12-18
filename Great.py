30e2341ac4e75a6746ad00870c0ea647edac75d3
WMGT Wanninayake <127291805+GayaniTharu@users.noreply.github.com>
2023-12-18 09:56:13 +0530

    Add files via upload

---
+++ Great.py
@@ -0,0 +1,8 @@
+# Get user input for their name
+user_name = input("Hey! What is your name? ")
+
+# Generate a greeting using the provided name
+greeting = f"Hello, {user_name}! Nice to meet you."
+
+# Print the greeting
+print(greeting)
